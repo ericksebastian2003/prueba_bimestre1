c1cc9efc748f48242a276991853c593455139c70
ericksebastian2003 <151951589+ericksebastian2003@users.noreply.github.com>
2024-11-03 20:02:57 -0500

    Nuevas actualizaciones

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,25 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'pruebab1',
-  webDir: 'www'
+  appName: 'Prueba-B1',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#000000",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#000000",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { SplashScreen } from '@capacitor/splash-screen';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+ //Función asincrónica para que se muestre el splash screen
+  async showSplashScreen(){
+    await SplashScreen.show({
+      showDuration: 4000,
+      autoHide: true,
+    });
+
+  }
 }
